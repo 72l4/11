e9c7b8d2b085b0ab36c32e5da54f618d8eea11fe
72l4 <loaysaid2025@gmil.com>
2026-03-12 22:37:13 +0400

    Fix comment formatting in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa'; // تم استيراد المكتبة
+import { VitePWA } from 'vite-plugin-pwa'; //  استيراد المكتبة
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
